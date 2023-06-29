fb5d2ad4ee255a221464eed4541849e8c0fb5379
Alan Diaz Lamberti <alan.diazlamberti@gmail.com>
2023-06-28 21:21:44 -0300

    funcion ver mas recetas

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,4 @@
+// Funcion para validar contacto
 function validarForm() {
   var nombre = document.getElementsByName("nombre")[0].value;
   var email = document.getElementsByName("email")[0].value;
@@ -9,4 +10,13 @@ function validarForm() {
   }
 
   return true;
+}
+
+// Funcion para ver mas recetas
+function verMas() {
+  var columnasOcultas = document.getElementsByClassName("hidden");
+
+  for (var i = 0; i < columnasOcultas.length; i++) {
+    columnasOcultas[i].classList.remove("hidden");
+  }
 }
