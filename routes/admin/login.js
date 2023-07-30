21c2735e96db1511578372eee70a161b9203d89f
Alan Diaz Lamberti <alan.diazlamberti@gmail.com>
2023-07-30 19:50:56 -0300

    agrego post para capturar datos login e importo usuariosModel

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,10 +1,33 @@
 var express = require("express");
 var router = express.Router();
+var usuariosModel = require("../../database/usuariosModel");
 
 router.get("/", function (req, res, next) {
   res.render("admin/login", {
     layout: "admin/layoutAdmin",
   });
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+
+    var data = await usuariosModel.getUserByUsernameAndPassword;
+
+    if (data != undefined) {
+      req.session.id_user = data.ID;
+      req.session.nombre = data.username;
+      res.redirect("admin/");
+    } else {
+      res.render("admin/login", {
+        layout: "admin/layoutAdmin",
+        error: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
